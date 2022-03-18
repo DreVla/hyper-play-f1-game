1d05084df18c5b983bdef8098ab1427705747013
Vlad Dreghici <vlad.dreghici@gmail.com>
2022-03-18 23:00:07 +0100

    F1 free assets & Joystick controller

---
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public float initialDrag;
+    public VariableJoystick variableJoystick;
+    public Rigidbody2D rb;
+
+    private void Start()
+    {
+        initialDrag = rb.drag;
+    }
+
+    public void FixedUpdate()
+    {
+        if (variableJoystick.Vertical == 0 && variableJoystick.Horizontal == 0)
+        {
+            rb.drag += Time.fixedDeltaTime;
+        }
+        else
+        {
+            rb.drag = initialDrag;
+            Vector2 direction = Vector2.up * variableJoystick.Vertical + Vector2.right * variableJoystick.Horizontal;
+            rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode2D.Impulse);
+        }
+    }
+}
