7cc40ad4060c53ed983ac47c3d9f506649a01b2c
Jessica Engstler <engstlerjessica@gmail.com>
2022-03-19 16:25:35 +0100

    tileset v2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,19 +3,5 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    bool gameIsOver = false;
-    public GameObject gameOverOverlay;
-    public void GameEnd()
-    {
-        if(!gameIsOver)
-        {
-            gameIsOver = true;
-            gameOverOverlay.SetActive(true);
-        }
-    }
 
-    void newGame()
-    {
-        SceneManager.LoadScene("Joystick Scene");
-    }
 }
