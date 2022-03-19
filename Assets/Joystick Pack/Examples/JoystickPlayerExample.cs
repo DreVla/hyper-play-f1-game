c5d75b5cd9d114bfef9a7ce91eecdfc99f2395ac
MichelePerrino <micheleperrino97@gmail.com>
2022-03-19 14:13:19 +0100

    Improved Controller
    Edited Joystick Player Example script;
    Added PitStop prefab;
    Edited Path Generator script.

--- Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -5,26 +5,16 @@ using UnityEngine;
 public class JoystickPlayerExample : MonoBehaviour
 {
     public float speed;
-    public float initialDrag;
+    public float initialDrag = 2;
     public VariableJoystick variableJoystick;
     public Rigidbody2D rb;
 
-    private void Start()
-    {
-        initialDrag = rb.drag;
-    }
+
 
     public void FixedUpdate()
     {
-        if (variableJoystick.Vertical == 0 && variableJoystick.Horizontal == 0)
-        {
-            rb.drag += Time.fixedDeltaTime;
-        }
-        else
-        {
             rb.drag = initialDrag;
             Vector2 direction = Vector2.up * variableJoystick.Vertical + Vector2.right * variableJoystick.Horizontal;
-            rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode2D.Impulse);
-        }
+            rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode2D.Impulse); 
     }
 }
--- Assets/Scripts/PathGenerator.cs
+++ Assets/Scripts/PathGenerator.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PathGenerator : MonoBehaviour
 {
     public GameObject[] pathTemplates;
+    public GameObject pitStop;
     public float timer = 10.0f;
+    public int pathsInstantiated = 0;
 
     void Start() {
         int rand = Random.Range(0, pathTemplates.Length);
@@ -14,9 +16,14 @@ public class PathGenerator : MonoBehaviour
 
     void Update() {
         timer -= Time.deltaTime;
-        if(timer <= 0f) {
+        if(timer <= 0f && pathsInstantiated < 3) {
             int rand = Random.Range(0, pathTemplates.Length);
             Instantiate(pathTemplates[rand], transform.position, Quaternion.identity);
+            pathsInstantiated++;
+            timer = 10.0f;
+        }
+        else if(timer <= 0f && pathsInstantiated == 1) {
+            Instantiate(pitStop);
             timer = 10.0f;
         }
     }    
