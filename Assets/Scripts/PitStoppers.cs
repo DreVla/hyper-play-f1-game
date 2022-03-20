27261de3f591e244278ebd4fec6bd6aad47e8338
MichelePerrino <micheleperrino97@gmail.com>
2022-03-20 12:06:20 +0100

    Pit Stoppers Rotation
    Edited prefab of the Pit Stopper;
    Added script rotating the pit stoppers towards the car.

---
+++ Assets/Scripts/PitStoppers.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitStoppers : MonoBehaviour
+{
+    private Transform target;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        target = GameObject.FindWithTag("Car").transform;
+        Debug.Log(target);
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90;
+        rb.rotation = angle;
+    }
+}
