24ca05fd88627b0c548c1f873f58f4fdd3786673
MichelePerrino <micheleperrino97@gmail.com>
2022-03-20 18:12:25 +0100

    easyPaths

--- Assets/Scripts/PathGenerator.cs
+++ Assets/Scripts/PathGenerator.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PathGenerator : MonoBehaviour
 {
     public GameObject[] pathTemplates;
+    public GameObject[] easypathTemplates;
+    public GameObject easyPitStop;
     public GameObject pitStop;
     public GameObject startingPath;
     public float timer = 2.0f;
@@ -18,18 +20,34 @@ public class PathGenerator : MonoBehaviour
 
     void Update() {
         timer -= Time.deltaTime;
-        
-        if(timer <= 0.0f) {
-            if(pathsInstantiated % 5 == 0 && pathsInstantiated != 0) {
-                Instantiate(pitStop, transform.position, Quaternion.identity);
-                timer = 2.0f;
-                pathsInstantiated++;
+        if (pathsInstantiated <= 10) {
+            if(timer <= 0.0f) {
+                if(pathsInstantiated % 5 == 0 && pathsInstantiated != 0) {
+                    Instantiate(easyPitStop, transform.position, Quaternion.identity);
+                    timer = 2.0f;
+                    pathsInstantiated++;
+                }
+                else {
+                    int rand = Random.Range(0, easypathTemplates.Length);
+                    Instantiate(easypathTemplates[rand], transform.position, Quaternion.identity);
+                    timer = 2.0f;
+                    pathsInstantiated++;
+                }
             }
-            else {
-                int rand = Random.Range(0, pathTemplates.Length);
-                Instantiate(pathTemplates[rand], transform.position, Quaternion.identity);
-                timer = 2.0f;
-                pathsInstantiated++;
+        }        
+        else if (pathsInstantiated > 10) {
+            if(timer <= 0.0f) {
+                if(pathsInstantiated % 5 == 0 && pathsInstantiated != 0) {
+                    Instantiate(pitStop, transform.position, Quaternion.identity);
+                    timer = 2.0f;
+                    pathsInstantiated++;
+                }
+                else {
+                    int rand = Random.Range(0, pathTemplates.Length);
+                    Instantiate(pathTemplates[rand], transform.position, Quaternion.identity);
+                    timer = 2.0f;
+                    pathsInstantiated++;
+                }
             }
         }
     }    
